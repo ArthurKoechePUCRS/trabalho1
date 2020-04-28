029a02e233d3f33a78b51f1dd8004a23025c1925
ArthurKoechePUCRS <64382106+ArthurKoechePUCRS@users.noreply.github.com>
2020-04-28 01:38:13 -0300

    Update Produto.cpp

--- Produto.cpp
+++ Produto.cpp
@@ -28,7 +28,48 @@ void Produto::defineCodBarras(string codBarras){
 
 string Produto::obtemDescricao(){
 
-
-
+        return descicao;
     
+}
+
+void defineDescricao( string descricao){
+
+        this->descricao=descricao;
+        
+} 
+int obtemGranalOuUnidade(){
+
+        return granalOuUnidade;
+}
+void defineGranelOuUnidade(int granelOuUnidade){
+
+        this->granelOuUnidade = granelOuUnidade;
+
+}
+string obtemMedida(){
+
+        return medida;
+        
+}
+void defineQuantidade(double quantidade){
+
+        this->quantidade = quantidade;
+        
+}
+double obtemPreco (){
+
+        return preco;
+}
+void definePreco (double preco){
+
+        this->preco = preco;
+
+}
+string toString(){
+
+        
+
+        
+
+
 }
