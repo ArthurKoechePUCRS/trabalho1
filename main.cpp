6ad5facb572517c4b8e298bcdeaa5f36c6574618
ArthurKoechePUCRS <64382106+ArthurKoechePUCRS@users.noreply.github.com>
2020-05-01 23:15:26 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,72 @@
 
 int main() {
 
+char obs;
 Produto temp;
 
-temp.defineCodBarras("123412312");
-temp.defineDescricao("Batata Doce"); 
-temp.defineGranelOuUnidade(1);
-temp.defineMedida("1 kg");
-temp.defineQuantidade(1);
-temp.definePreco (0.32);
+//temp.defineCodBarras("123412312");
+//temp.defineDescricao("Batata Doce"); 
+//temp.defineGranelOuUnidade(1);
+//temp.defineMedida("kg");
+//temp.defineQuantidade(1);
+//temp.definePreco (0.32);
 
-temp.toString() ;
+
+temp.fromCSV("123412312;Batata Doce;1;1 kg;1;0.32");
+//temp.toString();
+temp.toCSV();
+
+
+
+
+cout << "Seja Bem Vindo ao Registro do Mercado" << endl;
+
+cout << "Deseja ver o relatorio do mercado? (1)" << endl;
+cout << "Deseja editar algum produto do relatorio? (2)" << endl;
+cout << "Deseja adcionar algum produto ao mercado? (3)" << endl;
+cout << "Deseja remover algum produto do mercado? (4)" << endl;
+cout << "Deseja editar os dados do mercado? (5)" ;
+cin >> obs;
+
+switch (obs)
+{
+case 1:
+    cout << "CÓD.DE BARRAS DESCRIÇÃO UN./GR. MEDIDA EMBAL. PREÇO VALOR/MEDIDA   " << endl;
+    cout << "===================================================================" << endl;
+    
+    break;
+case 2:
+    //Edita produto;
+    break;
+case 3:
+    //Adciona produto;
+    break;
+case 4:
+    //Remove produto
+    break;
+case 5:
+    //Edita dados do mercado;
+    break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 return 0;
 }
+
